c380b6eacede9ed7ca822a278b628f439b1d3cf3
xianbuzhaoji-jpg <3093592364@qq.com>
2026-03-25 20:27:18 +0800

    Add main application entry point

---
+++ src/main/java/com/example/hablog/HablogApplication.java
@@ -0,0 +1,9 @@
+package com.example.hablog;  
+import org.springframework.boot.SpringApplication;  
+import org.springframework.boot.autoconfigure.SpringBootApplication;  
+@SpringBootApplication  
+public class HablogApplication {  
+    public static void main(String[] args) {  
+        SpringApplication.run(HablogApplication.class, args);  
+    }  
+}
